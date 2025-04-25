9c7aac7c6eb6c32319c9c2acbb7c6f0962c9e00f
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2025-04-24 23:28:21 -0300

    feat: conditional statements and loops

--- src/chapter-1/05-EqualsOperators.js
+++ src/chapter-1/05-EqualsOperators.js
@@ -17,32 +17,3 @@ if (num === 1) {
 }
 
 (num === 1) ? num-- : num++;
-
-var month = 5;
-if (month === 1) {
-    console.log('January');
-}
-else if (month === 2) {
-    console.log('February');
-}
-else if (month === 3) {
-    console.log('March');
-}
-else {
-    console.log('Month is not January, February, or March');
-}
-
-var month = 5;
-switch (month) {
-    case 1:
-        console.log('January');
-        break;
-    case 2:
-        console.log('February');
-        break;
-    case 3:
-        console.log('March');
-        break;
-    default:
-        console.log('Month is not January, February, or March');
-}
---
+++ src/chapter-1/06-ConditionalStatements.js
@@ -0,0 +1,28 @@
+var month = 5;
+if (month === 1) {
+    console.log('January');
+}
+else if (month === 2) {
+    console.log('February');
+}
+else if (month === 3) {
+    console.log('March');
+}
+else {
+    console.log('Month is not January, February, or March');
+}
+
+var month = 5;
+switch (month) {
+    case 1:
+        console.log('January');
+        break;
+    case 2:
+        console.log('February');
+        break;
+    case 3:
+        console.log('March');
+        break;
+    default:
+        console.log('Month is not January, February, or March');
+}
