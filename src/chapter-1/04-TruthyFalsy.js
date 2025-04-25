d0b35a271f9e481be47afb2368b39cff5356a8bb
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2025-04-24 23:15:53 -0300

    feat: more examples truthy false

--- src/chapter-1/04-TruthyFalsy.js
+++ src/chapter-1/04-TruthyFalsy.js
@@ -18,3 +18,14 @@ var obj = { name: 'John' };
 testTruthy(obj); // truthy
 testTruthy(obj.name); // falsy
 testTruthy(obj.age); // falsy
+
+console.log('packt' ? true : false); // true
+console.log('packt' == true); // false
+console.log('packt' == false); // false
+console.log('packt' === true); // false
+console.log('packt' === 'packt'); // true
+
+var person1 = { name: 'John' };
+var person2 = { name: 'John' };
+
+console.log(person1 == person2); // false
